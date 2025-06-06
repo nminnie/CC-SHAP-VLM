9e4980d0fa35590ec6df4fc3315167c3424bf1ec
Minnie Nguyen <minniexnguyen@gmail.com>
2025-06-06 11:07:16 +0100

    Specify params to load NLLB model

--- translate.py
+++ translate.py
@@ -1,3 +1,4 @@
+import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 lang_codes = {
@@ -11,15 +12,22 @@ lang_codes = {
     "zh": "zho_Hans"  # Simplified Chinese
 }
 
-def load_translation_model(model_name="facebook/nllb-200-3.3B"):
+model_path = "/work/tc067/tc067/s2737499/.cache/huggingface/hub/models--facebook--nllb-200-1.3B/snapshots/b0de46b488af0cf31749cd8da5ed3171e11b2309"
+def load_translation_model(model_name=model_path):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.float16,
+        device_map="auto",
+        low_cpu_mem_usage=True
+    )
 
     return model, tokenizer
 
 
 def translate_text(model, tokenizer, text, src_lang, tgt_lang="en"):
-    inputs = tokenizer(text, return_tensors="pt", src_lang=lang_codes[src_lang])
+    tokenizer.src_lang = lang_codes[src_lang]
+    inputs = tokenizer(text, return_tensors="pt")
     output = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id[lang_codes[tgt_lang]])
     translation = tokenizer.decode(output[0], skip_special_tokens=True)
 
